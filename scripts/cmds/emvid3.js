cfc5ab31f40f753ddcfc4f475d671359e92a1c20
psmaruf <mdsaniamarmama@gmail.com>
2025-09-14 20:13:01 +0600

    Update and rename rxjibon.js to emvid3.js

--- scripts/cmds/emvid3.js
+++ scripts/cmds/emvid3.js
@@ -1,6 +1,6 @@
 module.exports = {
   config: {
-    name: "rxjibon",
+    name: "emvid3",
     version: "1.0",
     author: "Goku",
     countDown: 5,
@@ -17,7 +17,7 @@ module.exports = {
       const video = "https://files.catbox.moe/g1wej3.mp4";
 
       return message.reply({
-        body: " 𝐉𝐈𝐁𝐎𝐍 𝐏𝐑𝐄𝐌𝐈𝐔𝐌💫𝐓𝐇𝐈𝐒 𝐐𝐔𝐀𝐋𝐈𝐓𝐘🎬",
+        body: " PS MARUF GAMING🎬",
         attachment: await global.utils.getStreamFromURL(video)
       });
     }
