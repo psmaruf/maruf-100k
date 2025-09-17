08aa822630661e679692934c6b99393e56c90ef7
psmaruf <mdsaniamarmama@gmail.com>
2025-09-18 02:22:58 +0600

    Update vaudio.js

--- scripts/cmds/vaudio.js
+++ scripts/cmds/vaudio.js
@@ -63,7 +63,7 @@ module.exports = {
                 }
                 else if (annoyedEmojis.some(emoji => msg.includes(emoji))) {
                     return message.reply({
-                        attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/l7plta.mp3"),
+                        attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/36hmkh.bin"),
                         body: "🙄"
                     });
                 }
